fb6a526e4d5963cb59e0a2af9526f4dbbbb02147
aryanjain200425 <aryanrashijain@gmail.com>
2024-05-31 12:49:30 -0400

    code for running the server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+server.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
+io.on('connection', (socket) => {
+
+    console.log('A user connected:', socket.id);
+
+
+});
